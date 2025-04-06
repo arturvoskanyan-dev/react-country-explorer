110af788f0468f759e6e6e4e85c5128d2f5617ab
arturvoskanyan-dev <arturvoskanyan.dev@gmail.com>
2025-04-06 13:48:02 +0400

    Initial commit of BLL implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
+import { useEffect, useReducer } from "react"
+import {initState, reducer, getAllActionCreator} from "./store/store"
+import { API } from "./api/api"
 import './App.css'
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initState)
+
+  useEffect(() => {
+    API.getAll().then((res) => dispatch(getAllActionCreator(res)))
+  }, [])
 
   return (
     <>
---
+++ src/store/store.js
@@ -0,0 +1,22 @@
+const GET_ALL = "get-all";
+
+const initState = {
+    countries: []
+}
+
+const reducer = (state, action) => {
+    switch(action.type) {
+        case GET_ALL: 
+            return {
+                ...state,
+                countries: action.payload
+            }
+
+        default : 
+            return state
+    }
+}
+
+const getAllActionCreator = (res) => ({type : GET_ALL, payload : res.data})
+
+export {initState, reducer, getAllActionCreator}
